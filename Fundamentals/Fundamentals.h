251580afb21e0e8061cdd645cfffebf4cc841660
christian otkjær <christian.otkjaer@gmail.com>
2017-04-07 16:34:35 +0200

    Initial Commit

---
+++ Fundamentals/Fundamentals.h
@@ -0,0 +1,19 @@
+//
+//  Fundamentals.h
+//  Fundamentals
+//
+//  Created by Christian Otkjær on 07/04/17.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fundamentals.
+FOUNDATION_EXPORT double FundamentalsVersionNumber;
+
+//! Project version string for Fundamentals.
+FOUNDATION_EXPORT const unsigned char FundamentalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fundamentals/PublicHeader.h>
+
+
